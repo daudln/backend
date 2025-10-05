9046c22747278920c822c442c002a8b91b052670
daudln <daudnamayala@gmail.com>
2025-10-06 01:15:14 +0300

    update

--- psrs_recruitment_backend/settings.py
+++ psrs_recruitment_backend/settings.py
@@ -138,14 +138,14 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 AUTH_USER_MODEL = "psrs_recruitment_auth.User"
 
 REST_FRAMEWORK = {
-    # "DEFAULT_AUTHENTICATION_CLASSES": [
-    #     # "rest_framework.authentication.TokenAuthentication",
-    #     # "rest_framework.authentication.SessionAuthentication",
-    #     "rest_framework_simplejwt.authentication.JWTAuthentication",
-    # ],
-    # "DEFAULT_PERMISSION_CLASSES": [
-    #     "rest_framework.permissions.IsAuthenticated",
-    # ],
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework.authentication.TokenAuthentication",
+        "rest_framework.authentication.SessionAuthentication",
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+    ],
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.IsAuthenticated",
+    ],
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
     "PAGE_SIZE": 10,
     "COERCE_DECIMAL_TO_STRING": False,
